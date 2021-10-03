4469bd8cda6c84fe02371dceb06a81dadc423a45
Blo <anmaystre@gmail.Com>
2021-10-03 11:06:13 +0200

    correction derniere exercice

--- Exercices/SurFIL/app.js
+++ Exercices/SurFIL/app.js
@@ -13,6 +13,19 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
+// Inserer le code correspondant à l'affichage des tarifs
+
+router.get("/vagues", (req, res) => {
+
+  let jour = [];
+  let taille = [];
+
+  // Inserer le code correspondant à l'affichage des tailles de vagues 
+
+  res.render("vagues", { jour: jour, taille: taille });
+});
+
+
 app.listen(process.env.port || 3000);
 
 console.log("Running at Port 3000");
